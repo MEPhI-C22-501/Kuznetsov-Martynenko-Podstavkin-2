0b4ea39e83b8a5b87109d58e194e76516fb38146
transaero21 <transaero21@elseboot.ru>
2024-12-16 11:51:15 +0300

    fix: add `rising_edge(i_clk)` condition to ALU process

--- ALU.vhd
+++ ALU.vhd
@@ -46,36 +46,38 @@ architecture ALU_arch of ALU is
         variable result : std_logic_vector(31 downto 0);
     begin
         if(i_rst = '1') then
-            result := x"00000000";
-        elsif(i_manage = ADDI_OP or i_manage = ADD_OP) then
-            result := addition(i_first_operand, i_second_operand);
-        elsif(i_manage = SLTI_OP or i_manage = SLT_OP) then 
-            result := set_less_then(i_first_operand, i_second_operand);            
-        elsif(i_manage = SLTIU_OP) then 
-            result := set_less_then_unsigned(i_first_operand, i_second_operand);            
-        elsif(i_manage = XORI_OP or i_manage = XOR_OP) then
-            result := i_first_operand xor i_second_operand;            
-        elsif(i_manage = ORI_OP or i_manage = OR_OP) then
-            result := i_first_operand or i_second_operand;            
-        elsif(i_manage = ANDI_OP or i_manage = AND_OP) then 
-            result := i_first_operand and i_second_operand;            
-        elsif(i_manage = SLLI_OP or i_manage = SLL_OP) then 
-            result := shift_left_logic(i_first_operand, i_second_operand); 
-        elsif(i_manage = SRLI_OP or i_manage = SRL_OP) then 
-            result := shift_right_logic(i_first_operand, i_second_operand); 
-        elsif(i_manage = SRAI_OP or i_manage = SRA_OP) then
-            result := shift_right_arithmetic(i_first_operand, i_second_operand); 
-        elsif(i_manage = SUB_OP) then
-            result := substraction(i_first_operand, i_second_operand); 
-        elsif(i_manage = MUL_OP) then 
-            result := multiplication(i_first_operand, i_second_operand); 
-        elsif(i_manage = MULH_OP) then 
-            result := multiplication_high(i_first_operand, i_second_operand); 
-        elsif(i_manage = MULHSU_OP) then
-            result := multiplication_high_signed_unsigned(i_first_operand, i_second_operand);
-        elsif(i_manage = MULHU_OP) then
-            result := multiplication_high_unsigned(i_first_operand, i_second_operand);
+            o_result <= x"00000000";
+        elsif rising_edge(i_clk) then
+            elsif(i_manage = ADDI_OP or i_manage = ADD_OP) then
+                result := addition(i_first_operand, i_second_operand);
+            elsif(i_manage = SLTI_OP or i_manage = SLT_OP) then 
+                result := set_less_then(i_first_operand, i_second_operand);            
+            elsif(i_manage = SLTIU_OP) then 
+                result := set_less_then_unsigned(i_first_operand, i_second_operand);            
+            elsif(i_manage = XORI_OP or i_manage = XOR_OP) then
+                result := i_first_operand xor i_second_operand;            
+            elsif(i_manage = ORI_OP or i_manage = OR_OP) then
+                result := i_first_operand or i_second_operand;            
+            elsif(i_manage = ANDI_OP or i_manage = AND_OP) then 
+                result := i_first_operand and i_second_operand;            
+            elsif(i_manage = SLLI_OP or i_manage = SLL_OP) then 
+                result := shift_left_logic(i_first_operand, i_second_operand); 
+            elsif(i_manage = SRLI_OP or i_manage = SRL_OP) then 
+                result := shift_right_logic(i_first_operand, i_second_operand); 
+            elsif(i_manage = SRAI_OP or i_manage = SRA_OP) then
+                result := shift_right_arithmetic(i_first_operand, i_second_operand); 
+            elsif(i_manage = SUB_OP) then
+                result := substraction(i_first_operand, i_second_operand); 
+            elsif(i_manage = MUL_OP) then 
+                result := multiplication(i_first_operand, i_second_operand); 
+            elsif(i_manage = MULH_OP) then 
+                result := multiplication_high(i_first_operand, i_second_operand); 
+            elsif(i_manage = MULHSU_OP) then
+                result := multiplication_high_signed_unsigned(i_first_operand, i_second_operand);
+            elsif(i_manage = MULHU_OP) then
+                result := multiplication_high_unsigned(i_first_operand, i_second_operand);
+            end if;
+            o_result <= result;
         end if;
-        o_result <= result;
     end process;
 end architecture;
